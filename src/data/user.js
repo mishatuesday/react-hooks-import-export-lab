cc8a81c5ffde35921aa761c0b0fa4a63c187f2e9
Misha Tuesday <mishatuesday@gmail.com>
2022-10-10 15:35:23 -0400

    commit tree 086064b2883ef4440f124c393e46da4e39a84d43

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,6 @@
-const username = "Liza";
-const city = "New York";
+const username = "Misha";
+const city = "Ypsilanti";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
-// export all three variables as named exports
+export {username, city, image}
